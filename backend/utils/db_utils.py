3a3763b569c49eab347aaeafefea85229a238831
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2025-11-03 19:34:13 +0100

    pooling credentials

--- backend/utils/db_utils.py
+++ backend/utils/db_utils.py
@@ -5,11 +5,11 @@ import time
 
 # Load environment variables from .env
 load_dotenv()
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER_POOL")
+DB_PASSWORD = os.getenv("DB_PASSWORD_POOL")
+DB_HOST = os.getenv("DB_HOST_POOL")
+DB_PORT = os.getenv("DB_PORT_POOL")
+DB_NAME = os.getenv("DB_NAME_POOL")
 
 
 class DBUtils:
